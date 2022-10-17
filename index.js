1261fc0c3f799d6e68846aa40e607aa3474988bb
Joshua Doud <bigjj3393@yahoo.com>
2022-10-17 00:58:26 -0700

    commit tree 095188bab74020b893e6797289380db282b2ff80

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: `Fred`,
+    streetAddress: `1111 Bedrock Way, Stonetown, FL 01234`
+}
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    return {
+    ...employeeObject,
+    [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value;
+    return employeeObject;
+}
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+    const newEmployee = {...employeeObject};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key]
+    return employeeObject
+}
